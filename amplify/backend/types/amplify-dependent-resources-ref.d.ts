d654518b989efaaaf7d8c8e7baa660cba66210a9
zeeshan <zeeshan.mustafa@emerald-labs.com>
2023-01-30 14:22:53 +0500

    Added graphql and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "imagestroge": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
